0afcda113fd91c161a6143b7285ae568444b4904
Andrea Tasso <andrea.tasso@ascom.com>
2023-11-04 10:24:05 +0100

    Changed wezterm config

--- wezterm/.wezterm.lua
+++ wezterm/.wezterm.lua
@@ -21,14 +21,78 @@ end
 config.color_scheme_dirs = { '../tokyonight/wezterm' }
 config.color_scheme = 'tokyonight_moon'
 if wezterm.target_triple == 'x86_64-pc-windows-msvc' then
-    config.default_prog = powershell
-    config.font_size = 9
+  config.default_prog = powershell
+  config.font_size = 9
 end
 config.leader = { key = 'a', mods = 'CTRL', timeout_milliseconds = 1000 }
 config.exit_behavior = 'Hold'
+config.window_frame = {
+  active_titlebar_bg = '#2f334d',
 
+  inactive_titlebar_bg = '#2f334d',
+}
 
+config.colors = {
+  tab_bar = {
+    -- The color of the strip that goes along the top of the window
+    -- (does not apply when fancy tab bar is in use)
+    inactive_tab_edge = "#2f334d",
 
+    -- The active tab is the one that has focus in the window
+    active_tab = {
+      fg_color = "#c8d3f5",
+      bg_color = "#222436",
+
+      -- Specify whether you want "Half", "Normal" or "Bold" intensity for the
+      -- label shown for this tab.
+      -- The default is "Normal"
+      intensity = 'Normal',
+
+      -- Specify whether you want "None", "Single" or "Double" underline for
+      -- label shown for this tab.
+      -- The default is "None"
+      underline = 'None',
+
+      -- Specify whether you want the text to be italic (true) or not (false)
+      -- for this tab.  The default is false.
+      italic = false,
+
+      -- Specify whether you want the text to be rendered with strikethrough (true)
+      -- or not for this tab.  The default is false.
+      strikethrough = false,
+    },
+
+    -- Inactive tabs are the tabs that do not have focus
+    inactive_tab = {
+      fg_color = "#545c7e",
+      bg_color = "#2f334d",
+
+      -- The same options that were listed under the `active_tab` section above
+      -- can also be used for `inactive_tab`.
+    },
+
+    -- You can configure some alternate styling when the mouse pointer
+    -- moves over inactive tabs
+    inactive_tab_hover = {
+      bg_color = "#2f334d",
+      fg_color = "#c8d3f5",
+      italic = true,
+
+      -- The same options that were listed under the `active_tab` section above
+      -- can also be used for `inactive_tab_hover`.
+    },
+
+  new_tab = {
+      fg_color = "#545c7e",
+      bg_color = "#2f334d",
+    },
+  new_tab_hover = {
+      bg_color = "#2f334d",
+      fg_color = "#c8d3f5",
+    },
+
+  },
+}
 config.keys = {
   {
     key = 'w',
@@ -45,7 +109,7 @@ config.keys = {
     mods = 'LEADER',
     action = act.SplitVertical { },
   },
-{
+  {
     key = 'LeftArrow',
     mods = 'ALT',
     action = act.ActivatePaneDirection 'Left',
@@ -65,7 +129,7 @@ config.keys = {
     mods = 'ALT',
     action = act.ActivatePaneDirection 'Down',
   },
- {
+  {
     key = 'LeftArrow',
     mods = 'SHIFT|ALT',
     action = act.AdjustPaneSize { 'Left', 5 },
