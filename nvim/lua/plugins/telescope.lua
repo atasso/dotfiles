190864f9c52e1aac97ea57e26f1368acc581e71e
Andrea Tasso <andrea.tasso@ascom.com>
2023-11-06 16:06:59 +0100

    changed telescope configuration

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,20 +1,23 @@
-
 return {
   'nvim-telescope/telescope.nvim', tag = '0.1.4',
   dependencies = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'} },
   keys = {
-    { '<C-p>', function() require 'telescope.builtin'.find_files() end, desc= "Fuzy find files"},
-    { '<leader>fg', function() require  'telescope.builtin'.live_grep() end, desc="Find in file"},
-    { '<leader>fb', function() require  'telescope.builtin'.buffers({initial_mode='normal'}) end, desc="List buffers"},
-    { '<leader>fr', function() require 'telescope.builtin'.lsp_references({initial_mode='normal'}) end, desc="List refernces"},
+    { '<C-p>', function() require 'config.telescopePickers'.prettyFilesPicker({picker = 'find_files'}) end, desc= "Fuzy find files"},
+    { '<leader>fg', function() require  'config.telescopePickers'.prettyGrepPicker({picker = 'live_grep'}) end, desc="Find in file"},
+    { '<leader>fb', function() require  'config.telescopePickers'.prettyBuffersPicker( {initial_mode='normal'}) end, desc="List buffers"},
+    { '<leader>fm', function() require  'config.telescopePickers'.prettyMarksPicker( {initial_mode='normal'}) end, desc="List buffers"},
+    { '<leader>fr', function() require 'config.telescopePickers'.prettyReferencePicker({initial_mode='normal'}) end, desc="List refernces"},
+    { 'gr', function() require 'config.telescopePickers'.prettyReferencePicker({initial_mode='normal'}) end, desc="List refernces"},
     { '<leader>fs', function() require 'telescope.builtin'.lsp_document_symbols() end, desc="List document symbols"},
-    { 'gr', function() require 'telescope.builtin'.lsp_references({initial_mode='normal'}) end, desc="List refernces"},
     { '<leader>ff', function() require 'telescope.builtin'.resume() end, desc="Resume last finder"},
     { '<leader>gs', function() require 'telescope.builtin'.git_status({initial_mode='normal'}) end, desc="List git status"},
     { '<leader>gb', function() require 'telescope.builtin'.git_branches() end, desc="List git branches"}
   },
   opts = {
     defaults = {
+      preview = {
+        filesize_limit = 0.1,
+      },
       mappings = {
         n = {
           ['<C-d>'] = "delete_buffer"
